1e4593ae0026519c478f8ec50b6067261b26e459
unnat <udas4153@gmail.com>
2024-05-03 21:09:25 +0530

    merge

---
+++ src/pages/space/room/index.js
@@ -0,0 +1,26 @@
+/** @format */
+
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const index = () => {
+   return (
+      <div>
+         <Tabs
+            defaultValue="stream"
+            className="w-full"
+         >
+            <TabsList>
+               <TabsTrigger value="stream">Stream</TabsTrigger>
+               <TabsTrigger value="classwork">Classwork</TabsTrigger>
+               <TabsTrigger value="people">people</TabsTrigger>
+               <TabsTrigger value="grade">grade</TabsTrigger>
+            </TabsList>
+            <TabsContent value="stream">Make changes to your account here.</TabsContent>
+            <TabsContent value="classwork">Change your password here.</TabsContent>
+         </Tabs>
+      </div>
+   );
+};
+
+export default index;
