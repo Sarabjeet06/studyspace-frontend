4119a788edea2ecc05899083c9ba4675ae9dcf87
yusira01 <yuvrajr.99@gmail.com>
2024-05-07 20:08:25 +0530

    home page reworked and other stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,17 @@ import Navbar from "@/components/Navbar";
 import Introduction from "@/components/Introduction";
 import AboutUs from "@/components/AboutUs";
 import Footer from "@/components/Footer";
+import Features from "@/components/Features";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-[#fdead3] overflow-hidden">
+    <div className="bg-[#dff6e7] overflow-hidden">
       <Navbar/>
       <Introduction/>
       <AboutUs/>
+      <Features/>
       <Footer/>
     </div>
   );
