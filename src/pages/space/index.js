c7df886e28841109f378aedd874fcd5f94a556da
yusira01 <yuvrajr.99@gmail.com>
2024-05-12 13:37:28 +0530

    some changes 2

--- src/pages/space/index.js
+++ src/pages/space/index.js
@@ -1,12 +1,27 @@
 import Card from '@/components/Card'
-import React from 'react'
+import Homenavbar from '@/components/Homenavbar'
+import Homesidemenubar from '@/components/Homesidemenubar'
+import React, { useState } from 'react'
 
 const index = () => {
+
+  const [menuClicked, setMenuClicked] = useState(false);
   return (
     <div>
-        <Card />
+      <Homenavbar menuClicked={menuClicked} setMenuClicked={setMenuClicked} />
+      <div className='w-full flex gap-0'>
+        <div className={`${menuClicked?'w-2/12': 'w-1/12'}`}>
+          <Homesidemenubar menuClicked={menuClicked} setMenuClicked={setMenuClicked} />
+        </div>
+        <div className={`${menuClicked?'w-10/12': 'w-11/12'}`} >
+          <Card />
+          <Card />
+          <Card />
+          <Card name = {"name"} />
+        </div>
+      </div>
     </div>
-    
+
   )
 }
 
