833963cd9bef86413cbb4a61e78098c5dd223ed4
Sukhbir Singh <sukhbir2506@outlook.com>
2024-05-12 18:45:47 +0530

    Updated Cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
+import { Button } from "@/components/ui/button"
 
-const Card = ({ name }) => {
+const Card = ({ className, classBtn, classDescription, ImagePath, ImageUser }) => {
     return (
         <div class="border max-w-80 mx-auto border-gray-300 rounded-md shadow-md">
             <div className='relative'>
-                <Image src="/images/CardTeacher.jpg" width={300} height={200} alt='Card Image' className='w-full rounded-md' />
-                <Image src="/images/User.jpg" width={100} height={50} alt='Card Image' className='rounded-full absolute -bottom-8 right-5 md:-bottom-8 md:right-10 md:z-50' />
+                <Image src={ImagePath} width={300} height={200} alt='Card Image' className='w-full rounded-md' />
+                <Image src={ImageUser} width={100} height={50} alt='Card Image' className='rounded-full absolute -bottom-8 right-5 md:-bottom-8 md:right-10 md:z-50' />
             </div>
-            <div class="p-4">
-                <h3 class="text-lg font-semibold mb-2">Sample Class Batch 1</h3>
-                <p class="text-gray-700 mb-4">Sample Description: Computer Science</p>
-                <a href="#" class="inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Read More</a>
+            <div className="p-4">
+                <h3 className="text-lg font-semibold mb-2">{className}</h3>
+                <p className="text-gray-700 mb-4">{classDescription}</p>
+                <a href="#" className="bg-blue-100">
+                <Button>{classBtn}</Button>
+                </a>
             </div>
         </div>
     )
