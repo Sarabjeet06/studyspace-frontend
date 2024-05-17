4018aec6b703c2ec9f2f2714b07ce054248ec456
unnat <udas4153@gmail.com>
2024-05-18 00:00:07 +0530

    minute change

--- src/components/HomePageLayout.js
+++ src/components/HomePageLayout.js
@@ -5,6 +5,8 @@ import { Popover, PopoverTrigger, PopoverContent } from "./ui/popover";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AnimatePresence, motion } from "framer-motion";
+import JoinClassModal from "./joinClassModal";
+import InviteUserModal from "./inviteUserModal";
 
 const HomePageLayout = ({ children }) => {
    const [menuClicked, setMenuClicked] = useState(false);
@@ -13,9 +15,10 @@ const HomePageLayout = ({ children }) => {
    const [expand2, setExpand2] = useState(true);
    const [expand3, setExpand3] = useState(true);
 
+   const [openJoinModal , setOpenJoinModal] = useState(false);
+   const [openCreateModal , setOpenCreateModal] = useState(false);
+
    const pathname = usePathname();
-   
-   console.log(pathname);
    return (
       <div className="w-full relative h-screen">
          <div className="z-[200] fixed md:relative w-full flex justify-between border border-b-2 border-gray-200 bg-white px-3 py-3 ">
@@ -58,9 +61,37 @@ const HomePageLayout = ({ children }) => {
                         </svg>
                      </div>
                   </PopoverTrigger>
-                  <PopoverContent>
-                     <div className="border-b-1 border-gray-300">Create class</div>
-                     <div>Join class</div>
+                  <PopoverContent className="w-full">
+                     <div className="text-sm">
+                        <div onClick={()=>{setOpenJoinModal(true)}} className="flex cursor-pointer my-2 items-center gap-2">
+                           <span>
+                              <svg
+                                 xmlns="http://www.w3.org/2000/svg"
+                                 height="19px"
+                                 viewBox="0 -960 960 960"
+                                 width="19px"
+                                 fill="#5f6368"
+                              >
+                                 <path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z" />
+                              </svg>
+                           </span>
+                           Join class
+                        </div>
+                        <div  onClick={()=>{setOpenJoinModal(true)}} className="flex cursor-pointer my-2 items-center gap-2">
+                           <span>
+                              <svg
+                                 xmlns="http://www.w3.org/2000/svg"
+                                 height="19px"
+                                 viewBox="0 -960 960 960"
+                                 width="19px"
+                                 fill="#5f6368"
+                              >
+                                 <path d="M500-482q29-32 44.5-73t15.5-85q0-44-15.5-85T500-798q60 8 100 53t40 105q0 60-40 105t-100 53Zm220 322v-120q0-36-16-68.5T662-406q51 18 94.5 46.5T800-280v120h-80Zm80-280v-80h-80v-80h80v-80h80v80h80v80h-80v80h-80Zm-480-40q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM0-160v-112q0-34 17.5-62.5T64-378q62-31 126-46.5T320-440q66 0 130 15.5T576-378q29 15 46.5 43.5T640-272v112H0Zm320-400q33 0 56.5-23.5T400-640q0-33-23.5-56.5T320-720q-33 0-56.5 23.5T240-640q0 33 23.5 56.5T320-560ZM80-240h480v-32q0-11-5.5-20T540-306q-54-27-109-40.5T320-360q-56 0-111 13.5T100-306q-9 5-14.5 14T80-272v32Zm240-400Zm0 400Z" />
+                              </svg>
+                           </span>
+                           Create class
+                        </div>
+                     </div>
                   </PopoverContent>
                </Popover>
 
@@ -95,7 +126,7 @@ const HomePageLayout = ({ children }) => {
                      pathname === "/space" ? "hover:bg-blue-200" : ""
                   } hover:bg-blue-100 hover:cursor-pointer item-center hover:rounded-md  px-2 py-2`}
                >
-                  <div className="flex items-center justify-center">
+                  <div className="flex cu items-center justify-center">
                      <svg
                         xmlns="http://www.w3.org/2000/svg"
                         height="24px"
@@ -108,7 +139,7 @@ const HomePageLayout = ({ children }) => {
                   </div>
                   {menuClicked && (
                      <motion.div
-                     className="md:block hidden"
+                        className="md:block hidden"
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
@@ -136,8 +167,7 @@ const HomePageLayout = ({ children }) => {
                   </div>
                   {menuClicked && (
                      <motion.div
-                     className="md:block hidden"
-
+                        className="md:block hidden"
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
@@ -461,7 +491,7 @@ const HomePageLayout = ({ children }) => {
                   )}
                </div>
             </div>
-     
+
             <div
                className={` ${
                   menuClicked ? "md:w-4/5 w-full" : "md:w-[95%] w-full"
@@ -470,6 +500,9 @@ const HomePageLayout = ({ children }) => {
                {children}
             </div>
             <div className="mt-16 block md:hidden w-full h-[90vh] overflow-y-auto">{children}</div>
+
+            <JoinClassModal open={openJoinModal} setOpen={setOpenJoinModal}/>
+            <InviteUserModal open={openCreateModal} setOpen={setOpenCreateModal}/>
          </section>
       </div>
    );
